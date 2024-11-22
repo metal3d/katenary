827b5bc830bcb0ad0c48115397831202fc83b591
Patrice Ferlet <metal3d@gmail.com>
2024-11-22 16:23:00 +0100

    test(values): add map-env and exchange volumes tests

--- generator/configMap_test.go
+++ generator/configMap_test.go
@@ -1,6 +1,8 @@
 package generator
 
 import (
+	"fmt"
+	"katenary/generator/labels"
 	"os"
 	"testing"
 
@@ -38,5 +40,36 @@ services:
 	}
 	if data["BAR"] != "baz" {
 		t.Errorf("Expected BAR to be baz, got %s", data["BAR"])
+	}
+}
+
+func TestMapEnv(t *testing.T) {
+	composeFile := `
+services:
+  web:
+    image: nginx:1.29
+    environment:
+      FOO: bar
+    labels:
+      %[1]s/map-env: |-
+        FOO: 'baz'
+`
+
+	composeFile = fmt.Sprintf(composeFile, labels.KatenaryLabelPrefix)
+	tmpDir := setup(composeFile)
+	defer teardown(tmpDir)
+
+	currentDir, _ := os.Getwd()
+	os.Chdir(tmpDir)
+	defer os.Chdir(currentDir)
+
+	output := internalCompileTest(t, "-s", "templates/web/configmap.yaml")
+	configMap := v1.ConfigMap{}
+	if err := yaml.Unmarshal([]byte(output), &configMap); err != nil {
+		t.Errorf(unmarshalError, err)
+	}
+	data := configMap.Data
+	if v, ok := data["FOO"]; !ok || v != "baz" {
+		t.Errorf("Expected FOO to be baz, got %s", v)
 	}
 }
