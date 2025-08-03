136478aff7cc8e07e832572a3bc24940afd15e51
Patrice Ferlet <metal3d@gmail.com>
2025-08-03 10:12:24 +0200

    fix(configmap): File from the root of project ends by a dash
    fixes #150
    
    If we mount a file from the root of project, the names ends by a dash.
    That makes helm failing to install the package.

--- generator/configMap_test.go
+++ generator/configMap_test.go
@@ -2,8 +2,10 @@ package generator
 
 import (
 	"fmt"
+	"io"
 	"katenary/generator/labels"
 	"os"
+	"regexp"
 	"testing"
 
 	"github.com/compose-spec/compose-go/types"
@@ -88,5 +90,43 @@ func TestAppendBadDir(t *testing.T) {
 	err := cm.AppendDir("foo")
 	if err == nil {
 		t.Errorf("Expected error, got nil")
+	}
+}
+
+func TestRootConfigmapfile(t *testing.T) {
+	composeFile := `
+services:
+  web:
+    image: nginx
+    volumes:
+      - ./foo.txt:/etc/foo.txt
+    labels:
+      %[1]s/configmap-files: |-
+        - ./foo.txt
+`
+	composeFile = fmt.Sprintf(composeFile, labels.KatenaryLabelPrefix)
+	tmpDir := setup(composeFile)
+	defer teardown(tmpDir)
+
+	currentDir, _ := os.Getwd()
+	os.Chdir(tmpDir)
+	defer os.Chdir(currentDir)
+	fooTxt := "foo content"
+	fooFp, _ := os.Create("foo.txt")
+	io.WriteString(fooFp, fooTxt)
+	fooFp.Close()
+
+	output := internalCompileTest(t, "-s", "templates/web/statics/configmap.yaml")
+	configMap := v1.ConfigMap{}
+	if err := yaml.Unmarshal([]byte(output), &configMap); err != nil {
+		t.Errorf(unmarshalError, err)
+	}
+	if configMap.Data == nil {
+		t.Error("Expected configmap data to not be nil")
+	}
+	// if the configmap.Name ends by anything that is not alphanumeric, there is a problem
+	valid := regexp.MustCompile(`.*[a-zA-Z0-9]+$`)
+	if !valid.MatchString(configMap.Name) {
+		t.Errorf("ConfigMap name %s is not valid", configMap.Name)
 	}
 }
